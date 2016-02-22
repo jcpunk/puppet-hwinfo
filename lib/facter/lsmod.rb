05830e81660d621feae663b4dc500deea339fe8e
Pat Riehecky <riehecky@fnal.gov>
2016-02-22 16:28:19 -0600

    Adding lots of extra information

--- lib/facter/lsmod.rb
+++ lib/facter/lsmod.rb
@@ -13,7 +13,8 @@
 #   The result is 
 #
 if Facter::Util::Resolution.which('lsmod')
-  retval = []
+  retval = {}
+  modinfo = ['filename', 'intree', 'srcversion', 'vermagic', 'version']
   Facter::Util::Resolution.exec("lsmod 2>/dev/null").each_line do |line|
     if not line =~ /.+/
       next
@@ -23,14 +24,39 @@ if Facter::Util::Resolution.which('lsmod')
     end
     matches = line.match(/(\S+)/)
     if matches
-      retval.push(matches[1])
+      modulename = matches[1].strip
+      retval[modulename] = {}
+      if Facter::Util::Resolution.which('modinfo')
+        for info in modinfo
+          retval[modulename][info] = Facter::Util::Resolution.exec("modinfo --field=#{info} #{modulename} 2>/dev/null").strip
+        end
+
+        retval[modulename]['parm'] = []
+        Facter::Util::Resolution.exec("modinfo --field=parm #{modulename} 2>/dev/null").each_line do |txt|
+          retval[modulename]['parm'].push(txt.strip)
+        end
+
+        retval[modulename]['alias'] = []
+        Facter::Util::Resolution.exec("modinfo --field=alias #{modulename} 2>/dev/null").each_line do |txt|
+          retval[modulename]['alias'].push(txt.strip)
+        end
+
+        retval[modulename]['depends'] = []
+        Facter::Util::Resolution.exec("modinfo --field=depends #{modulename} 2>/dev/null").each_line do |txt|
+          retval[modulename]['depends']= txt.strip.split(',')
+        end
+
+        if Facter::Util::Resolution.which('rpm')
+          retval[modulename]['RPM'] = Facter::Util::Resolution.exec("rpm -qf #{retval[modulename]['filename']} 2>/dev/null").strip
+        end
+      end
     end
   end
 
   Facter.add(:lsmod) do
     confine :kernel => "Linux"
     setcode do
-      retval.sort
+      retval
     end
   end
 end
