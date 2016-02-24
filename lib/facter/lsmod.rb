d329cc207a3fa10296568d47ff27ac9d0584fa0f
Pat Riehecky <riehecky@fnal.gov>
2016-02-24 15:44:59 -0600

    Cleaner output, debian support

--- lib/facter/lsmod.rb
+++ lib/facter/lsmod.rb
@@ -13,8 +13,9 @@
 #   The result is 
 #
 if Facter::Util::Resolution.which('lsmod')
-  retval = {}
-  modinfo = ['filename', 'intree', 'srcversion', 'vermagic', 'version']
+  tmphash = {}
+  finalhash = {}
+  modinfo = ['intree', 'srcversion', 'vermagic', 'version']
   t = []
   Facter::Util::Resolution.exec("lsmod 2>/dev/null").each_line do |line|
     if not line =~ /.+/
@@ -28,68 +29,107 @@ if Facter::Util::Resolution.which('lsmod')
       t.push(Thread.new {
         waitforit = []
         modulename = matches[1].strip
-        retval[modulename] = {}
+
+        tmphash[modulename] = {}
+
+        if Facter::Util::Resolution.which('modinfo')
+          # Can't do this in parallel, other things need it first
+          for info in modinfo
+            result = Facter::Util::Resolution.exec("modinfo --field='filename' #{modulename} 2>/dev/null").strip
+            if not result.empty?
+              tmphash[modulename]['filename'] = result
+            end
+          end
+        end
 
         if Facter::Util::Resolution.which('modinfo')
           waitforit.push(Thread.new {
             for info in modinfo
-              retval[modulename][info] = Facter::Util::Resolution.exec("modinfo --field=#{info} #{modulename} 2>/dev/null").strip
-            end
-          })
-
-          waitforit.push(Thread.new {
-            retval[modulename]['parm'] = {}
-            Facter::Util::Resolution.exec("grep '' /sys/module/#{modulename}/parameters/* 2>/dev/null").each_line do |txt|
-              if not txt =~ /.+:.+/
-                next
+              result = Facter::Util::Resolution.exec("modinfo --field=#{info} #{modulename} 2>/dev/null").strip
+              if not result.empty?
+                tmphash[modulename][info] = result
               end
-              path = txt.split(':')[0]
-              parm = path.split('/').last
-              value = txt.split(':')[1].strip
-              retval[modulename]['parm'][parm] = value
             end
-            retval[modulename]['parm'].sort
           })
 
-          #waitforit.push(Thread.new {
-          #  retval[modulename]['alias'] = []
-          #  Facter::Util::Resolution.exec("modinfo --field=alias #{modulename} 2>/dev/null").each_line do |txt|
-          #    retval[modulename]['alias'].push(txt.strip)
-          #  end
-          #  retval[modulename]['alias'].sort
-          #})
+          waitforit.push(Thread.new {
+            if File.directory?("/sys/module/#{modulename}/parameters")
+              tmphash[modulename]['parm'] = {}
+              Facter::Util::Resolution.exec("grep '' /sys/module/#{modulename}/parameters/* 2>/dev/null").each_line do |txt|
+                if not txt =~ /.+:.+/
+                  next
+                end
+                path = txt.split(':')[0]
+                parm = path.split('/').last
+                value = txt.split(':')[1].strip
+                tmphash[modulename]['parm'][parm] = value
+              end
+              tmphash[modulename]['parm'].sort
+            end
+          })
 
           waitforit.push(Thread.new {
-            retval[modulename]['depends'] = []
-            Facter::Util::Resolution.exec("modinfo --field=depends #{modulename} 2>/dev/null").each_line do |txt|
-              retval[modulename]['depends']= txt.strip.split(',')
+            mytaint = Facter::Util::Resolution.exec("cat /sys/module/#{modulename}/taint 2>/dev/null")
+            if not mytaint.empty?
+              if mytaint != 'Y'
+                tmphash[modulename]['taint'] = mytaint.strip
+              end
             end
-            retval[modulename]['depends'].sort
+          })
+
+          waitforit.push(Thread.new {
+            tmphash[modulename]['depends'] = []
+            Facter::Util::Resolution.exec("modinfo --field=depends #{modulename} 2>/dev/null").each_line do |txt|
+              tmphash[modulename]['depends']= txt.strip.split(',')
+            end
+            if tmphash[modulename]['depends'] == []
+              tmphash[modulename].delete('depends')
+            else
+              tmphash[modulename]['depends'].sort
+            end
           })
         end
 
         if Facter::Util::Resolution.which('rpm')
           waitforit.push(Thread.new {
+              realname = Facter::Util::Resolution.exec("readlink -f #{tmphash[modulename]['filename']} 2>/dev/null").strip
               # this is really slow
-              retval[modulename]['RPM'] = Facter::Util::Resolution.exec("rpm -qf #{retval[modulename]['filename']} 2>/dev/null").strip
+              if not realname.empty?
+                tmphash[modulename]['package'] = Facter::Util::Resolution.exec("rpm -qf #{realname} 2>/dev/null").strip
+              else
+                tmphash[modulename]['package'] = Facter::Util::Resolution.exec("rpm -qf #{tmphash[modulename]['filename']} 2>/dev/null").strip
+              end
+          })
+        elsif Facter::Util::Resolution.which('dpkg')
+          waitforit.push(Thread.new {
+              realname = Facter::Util::Resolution.exec("readlink -f #{tmphash[modulename]['filename']} 2>/dev/null").strip
+              # this is really slow
+              if not realname.empty?
+                mypkgname = Facter::Util::Resolution.exec("dpkg -s #{realname} 2>/dev/null").strip
+                tmphash[modulename]['package'] = mypkgname.split(':')[0]
+              else
+                mypkgname = Facter::Util::Resolution.exec("dpkg -s #{tmphash[modulename]['filename']} 2>/dev/null").strip
+                tmphash[modulename]['package'] = mypkgname.split(':')[0]
+              end
           })
         end
 
         for thread in waitforit
           thread.join
         end
-        retval[modulename].sort
+        tmphash[modulename].sort
       })
     end
     for thread in t
       thread.join
     end
+    finalhash = Hash[tmphash.sort]
   end
 
   Facter.add(:lsmod) do
     confine :kernel => "Linux"
     setcode do
-      retval.sort
+      finalhash
     end
   end
 end
